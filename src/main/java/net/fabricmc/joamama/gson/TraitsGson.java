8c7abbeefd2bef2cf16554ff5da31ec803f0c58f
bldhf <pascalbracamonte@gmail.com>
2024-05-30 16:40:01 -0400

    Floats and doubles get rounded at 6 decimal digits now. This breaks a few things, but for those few things... just comment it out, I guess.

--- src/main/java/net/fabricmc/joamama/gson/TraitsGson.java
+++ src/main/java/net/fabricmc/joamama/gson/TraitsGson.java
@@ -3,17 +3,28 @@ package net.fabricmc.joamama.gson;
 import com.google.common.collect.Table;
 import com.google.gson.*;
 import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Map;
+
+import net.fabricmc.joamama.JoaMama;
 import net.minecraft.world.entity.MobType;
 
 public class TraitsGson {
     private static final Gson gson = new GsonBuilder()
-            .registerTypeAdapter(Table.class, new TableSerializer())
-            .registerTypeAdapter(Map.class, new MapSerializer())
-            .excludeFieldsWithoutExposeAnnotation()
-            .disableHtmlEscaping()
-            .setPrettyPrinting()
-            .create();
+        .registerTypeAdapter(Table.class, new TableSerializer())
+        .registerTypeAdapter(Map.class, new MapSerializer())
+        // If you need to get exact numbers, comment these lines out.
+        // It also breaks things that use the magic horse width.
+        // Why the hell is the magic horse width such a specific number?
+        // Like, 7 goddamn decimal digits. Seriously!?
+        .registerTypeAdapter(Float.class, new NumberSerializer())
+        .registerTypeAdapter(Double.class, new NumberSerializer())
+        .excludeFieldsWithoutExposeAnnotation()
+        .disableHtmlEscaping()
+        .setPrettyPrinting()
+        .create();
 
     public static Gson gson() {
         return gson;
@@ -28,19 +39,18 @@ public class TraitsGson {
 
     @SuppressWarnings("rawtypes")
     private static class MapSerializer implements JsonSerializer<Map> {
-        private static final Gson mapGson = new GsonBuilder()
-                .registerTypeAdapter(Table.class, new TableSerializer())
-                .excludeFieldsWithoutExposeAnnotation()
-                .disableHtmlEscaping()
-                .setPrettyPrinting()
-                .create();
-
         public JsonElement serialize(Map source, Type type, JsonSerializationContext context) {
             if (source.size() == 1) {
                 return context.serialize(source.values().iterator().next());
             } else {
-                return mapGson.toJsonTree(source);
+                return gson.toJsonTree(source);
             }
+        }
+    }
+
+    private static class NumberSerializer implements JsonSerializer<Number> {
+        public JsonElement serialize(Number source, Type type, JsonSerializationContext context) {
+            return new JsonPrimitive(BigDecimal.valueOf(source.doubleValue()).setScale(6, RoundingMode.HALF_EVEN).stripTrailingZeros());
         }
     }
 }
